e3ca762f891525beafde37d2afb9e9845c3383cb
Logan <logan.ragland25@gmail.com>
2025-09-12 19:33:04 -0400

    Finished GetFirstInList

--- week_03/project_01/project_01.cpp
+++ week_03/project_01/project_01.cpp
@@ -50,16 +50,51 @@ size_t FindInList(std::string wordList, std::string word, size_t pos=0) {
     return std::string::npos;
 }
 
+
+std::string GetFirstInList(std::string wordList, std::string word1, std::string word2) {
+    int word1Pos{}, word2Pos{};
+    
+    word1Pos = FindInList(wordList, word1);
+    word2Pos = FindInList(wordList, word2);
+
+    if (word1Pos >= 0 && word2Pos >= 0) {
+        if (word1Pos < word2Pos) {
+            return word1;
+        }
+        else if (word1Pos > word2Pos) {
+            return word2;
+        }
+    }
+    else if ((word1Pos < 0 && word2Pos < 0) || word1Pos == word2Pos) {
+        return "N/A";
+    }
+    else if (word1Pos < 0) {
+        return word2;
+    }
+    else if (word2Pos < 0) {
+        return word1;
+    }
+    return "N/A";
+}
+
 int main()
 {
-    std::string word_list="A,list,of,words,to,use,for,testing";
-    std::cout << FindInList(word_list, "A");
-    // CHECK(FindInList(word_list, "list") == 2u);
-    // CHECK(FindInList(word_list, "of") == 7u);
-    // CHECK(FindInList(word_list, "words") == 10u);
-    // CHECK(FindInList(word_list, "to") == 16u);
-    // CHECK(FindInList(word_list, "use") == 19u);
-    // CHECK(FindInList(word_list, "for") == 23u);
-    // CHECK(FindInList(word_list, "testing") == 27u);
+    std::string word1 = "turtle";
+    std::string word2 = "hare";
+    
+    // std::string list = "this,is,a,race,between,a,turtle,and,a,hare";
+    // std::cout << GetFirstInList(list, word1, word2);
+    
+    std::string list = "sometimes,the,turtle,wins,even,though,everyone,expects,the,hare";
+    std::cout << GetFirstInList(list, word1, word2);// == "turtle", "Check 2" << "\n";
+    
+    list = "but,usually,its,the,hare";
+    std::cout << GetFirstInList(list, word1, word2); // == "hare", "Check 3" << "\n";
+    
+    list = "unless,the,turtle,races,itself";
+    std::cout << GetFirstInList(list, word1, word2); // == "turtle", "Check 4" << "\n";
+    
+    list = "hair,is,not,a,valid,word";
+    std::cout << GetFirstInList(list, word1, word2); // == "N/A", "Check 5" << "\n";
 
 }
